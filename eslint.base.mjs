b75423f97226eef156443ddd63a769bd6495a133
Sidney Keese <me@sidke.com>
2024-10-15 22:19:00 -0700

    sort keys lint rule

--- eslint.base.mjs
+++ eslint.base.mjs
@@ -10,9 +10,7 @@ export default {
       "warn",
       {
         "newlines-between": "always",
-        pathGroups: [
-          { pattern: "@/**", group: "parent" },
-        ],
+        pathGroups: [{ pattern: "@/**", group: "parent" }],
         alphabetize: { order: "asc" },
       },
     ],
@@ -23,5 +21,15 @@ export default {
         ignoreMemberSort: false,
       },
     ],
+    "sort-keys": [
+      "warn",
+      "asc",
+      {
+        caseSensitive: true,
+        minKeys: 2,
+        natural: false,
+      },
+    ],
+    "sort-keys-fix/sort-keys-fix": "warn",
   },
 };
--- packages/backend/eslint.config.js
+++ packages/backend/eslint.config.js
@@ -2,11 +2,17 @@ import tseslint from "typescript-eslint";
 import prettier from "eslint-plugin-prettier/recommended";
 import base from "../../eslint.base.mjs";
 import importPlugin from "eslint-plugin-import";
+import sortKeysFixPlugin from "eslint-plugin-sort-keys-fix";
 
 export default tseslint.config(
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
   importPlugin.flatConfigs.recommended,
+  {
+    plugins: {
+      "sort-keys-fix": sortKeysFixPlugin,
+    },
+  },
   prettier,
   base,
   {
